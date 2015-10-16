c289c22e48c775eb9015374417ba176c19d02961
nathanielgreen <ngreen2303@gmail.com>
2015-10-16 13:32:23 +0100

    Completed average_rating method. Tests passing apart from displaying average

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -12,7 +12,7 @@ class Restaurant < ActiveRecord::Base
 
   def average_rating
     return 'N/A' if reviews.none?
-    4
+    reviews.inject(0) {|memo, review| memo + review.rating} / reviews.length
   end
 
 end
