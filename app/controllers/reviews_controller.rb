a922ab58914970b08023c2b2cd4af4f2cb0df103
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-16 10:38:52 +0100

    can only delete reviews belonging to the user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,4 +26,11 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:thoughts, :rating)
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review deleted successfully"
+    redirect_to '/restaurants'
+  end
+
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -2,22 +2,77 @@ require 'rails_helper'
 
 feature 'reviewing' do
 
-  before do
-    user = build :user
-    sign_up(user)
-  end
+  context 'user is signed in' do
+    before do
+      user = build :user
+      sign_up(user)
+    end
 
-  scenario 'allows users to leave a review using a form' do
-    visit '/restaurants'
-    click_link('Add a restaurant')
-    fill_in 'Name', with: 'KFC'
-    click_button 'Create Restaurant'
-    click_link 'Review KFC'
-    fill_in "Thoughts", with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
+    scenario 'allows users to leave a review using a form' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('so so')
+    end
+
+    scenario 'can only leave one review per restaurant' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('so so')
+    end
+
+    scenario 'can only delete restaurant user create' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      expect(page).to have_link 'Delete KFC'
+    end
+
+    scenario 'can only delete restaurant user create' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      click_link 'Sign out'
+      user = build :user2
+      sign_up(user)
+      expect(page).not_to have_link 'Delete KFC'
+    end
+
+    scenario 'can delete review' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      expect(page).to have_content 'Delete review'
+    end
+
   end
 
 end
