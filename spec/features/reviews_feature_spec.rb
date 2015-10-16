3614e99c489c9819e5716386de72118d6e6caf38
nathanielgreen <ngreen2303@gmail.com>
2015-10-16 11:34:12 +0100

    Added feature and unit tests for average review ratings

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -72,7 +72,22 @@ feature 'reviewing' do
       click_button 'Leave Review'
       expect(page).to have_content 'Delete review'
     end
+    
+    scenario 'displays an average rating for all reviews' do
+      leave_review('So so', '3')
+      leave_review('Great', '5')
+      expect(page).to have_content('Average rating: 4')
+    end
 
   end
 
 end
+
+def leave_review(thoughts, rating)
+  visit '/restaurants'
+  click_link 'Review KFC'
+  fill_in 'Thoughts', with: thoughts
+  select rating, from: 'Rating'
+  click_button 'Leave Review'
+end
+
