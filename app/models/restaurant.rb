bf9f377a4bac84d6c755415839ed9283d995b39c
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-16 11:56:48 +0100

    create average_rating method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,4 +10,9 @@ class Restaurant < ActiveRecord::Base
     review
   end
 
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
+
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'reviewing' do
 
   context 'user is signed in' do
+
     before do
       user = build :user
       sign_up(user)
@@ -72,9 +73,26 @@ feature 'reviewing' do
       click_button 'Leave Review'
       expect(page).to have_content 'Delete review'
     end
-    
+
     scenario 'displays an average rating for all reviews' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      # click_link 'Review KFC'
+      # fill_in "Thoughts", with: "so so"
+      # select '3', from: 'Rating'
+      # click_button 'Leave Review'
       leave_review('So so', '3')
+      click_link 'Sign out'
+      user = build :user2
+      sign_up(user)
+      # click_link 'Review KFC'
+      # fill_in "Thoughts", with: "great"
+      # select '5', from: 'Rating'
+      # click_button 'Leave Review'
+      # fill_in 'Thoughts', with: thoughts
+      # select rating, from: 'Rating'
       leave_review('Great', '5')
       expect(page).to have_content('Average rating: 4')
     end
@@ -84,10 +102,8 @@ feature 'reviewing' do
 end
 
 def leave_review(thoughts, rating)
-  visit '/restaurants'
   click_link 'Review KFC'
   fill_in 'Thoughts', with: thoughts
   select rating, from: 'Rating'
   click_button 'Leave Review'
 end
-
