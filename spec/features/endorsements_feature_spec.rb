0a52f909243194c311880819d498aa1f47edbca6
nathanielgreen <ngreen2303@gmail.com>
2015-10-16 16:00:24 +0100

    added endorsements relations and associates and migrated to tables. number of endorsements shows nexts to reviews but no endorsement link yet

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'edorsing reviews' do
 
   scenario 'a user can endorse a review which updates the review endorsement count' do
     visit '/restaurants'
-    click_link 'Endorse Review'
+    click_link 'Endorse review'
     expect(page).to have_content('1 endorsement')
   end
 
