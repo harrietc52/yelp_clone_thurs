86de0e4bb90be5d522ee747adec12628c9c5d1cc
nathanielgreen <ngreen2303@gmail.com>
2015-10-16 13:56:11 +0100

    added test for 3 star rating

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -10,5 +10,10 @@ describe ReviewsHelper , :type => :helper do
     it 'returns five black starts for five' do
       expect(helper.star_rating(5)).to eq '★★★★★'
     end
+
+    it 'returns three black stars and two white stars for three' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
   end
 end
